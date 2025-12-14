7befe594e65fa1e051bda4947424ec1bda6482b6
muhammedalicelk <muhammedalicelk@gmail.com>
2025-12-14 18:12:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-// ============================================
-// 2. vite.config.js
-// ============================================
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
