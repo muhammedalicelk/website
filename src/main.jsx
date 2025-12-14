058e75e251ab0b27b4342e96ad72939d0f3163cc
muhammedalicelk <muhammedalicelk@gmail.com>
2025-12-14 18:14:31 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-// ============================================
-// 5. src/main.jsx
-// ============================================
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
