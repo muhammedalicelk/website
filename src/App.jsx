279805b88084254a14fa53550448674d76055e73
muhammedalicelk <muhammedalicelk@gmail.com>
2025-12-15 22:19:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ export default function SesliOyuncakSiparis() {
           {/* Müşteri Bilgileri */}
           <div className="mb-8">
             <h2 className="text-xl font-semibold text-gray-800 mb-4 flex items-center">
-              <User className="w-5 h-5 mr-2 text-purple-500" />
+              <User className="w-5 h-5 mr-2 text-amber-700" />
               İletişim Bilgileri
             </h2>
 
@@ -174,7 +174,7 @@ export default function SesliOyuncakSiparis() {
           {/* Müzik Seçimi */}
           <div className="mb-6">
             <h2 className="text-xl font-semibold text-gray-800 flex items-center mb-4">
-              <Music className="w-5 h-5 mr-2 text-purple-500" />
+              <Music className="w-5 h-5 mr-2 text-amber-700" />
               Müzik Seçimi *
             </h2>
 
